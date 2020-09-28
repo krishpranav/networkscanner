a0d9f6c5c1d75a7c16d6b8c3f484574bc9b975a1
krishpranav <krisna.pranav@gmail.com>
2020-09-28 17:31:40 +0530

    done changes

--- networkscan.py
+++ networkscan.py
@@ -6,16 +6,16 @@ import argparse              #imports
 import time
 
 #scan function
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    client_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        client_list.append(client_dict)
-    return client_list
+# def scan(ip):
+#     arp_request = scapy.ARP(pdst=ip)
+#     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+#     arp_request_broadcast = broadcast / arp_request
+#     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+#     client_list = []
+#     for element in answered_list:
+#         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+#         client_list.append(client_dict)
+#     return client_list
 
 #result printing
 def print_result(result_list):
